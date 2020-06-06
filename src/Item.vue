15c0ef7df0d2d2054b690b00455ea01edaa0d044
avijit <avijitd852@gmail.com>
2020-06-06 20:28:23 +0530

    eventBus concept

---
+++ src/Item.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="doJobsList">To See All jobs</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            doJobs:[
+                {id:1,jobName:'development', duration:'1 to 10 hr'},
+                {id:2,jobName:'teching', duration:'1 to 8 hr'},
+                {id:3,jobName:'coding', duration:'1 to 9 hr'},
+                {id:4,jobName:'driver', duration:'1 to 7 hr'}
+            ]
+        }
+    },
+    methods:{
+        doJobsList(){
+            eventBus.$emit('listItem',this.doJobs);
+        }
+    }   
+}
+</script>
---
+++ src/ListItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h3>This is my List Items</h3>
+        <div>
+            <table border="1" >
+                <tr>
+                    <td>ID</td>
+                    <td>Job Name</td>
+                    <td>Job Duration</td>
+                </tr>  
+                <tr v-if="foundData">
+                    <th colspan="3">Sorry no more data found</th>
+                </tr>    
+                <tr v-else v-for="item in listAll" :key="item.id">
+                    <td>{{item.id}}</td>
+                    <td>{{item.jobName}}</td>
+                    <td>{{item.duration}}</td>
+                </tr>
+                <tr>
+
+                </tr>
+            </table>
+        </div>
+        <Item></Item>
+    </div>
+</template>
+
+
+<script>
+import Item from './Item'
+export default {
+    components:{
+        Item,
+    },
+    data(){
+        return{
+            listAll:[],
+            foundData:true
+        }
+    },
+    created(){
+        eventBus.$on('listItem',(data)=>{
+            this.listAll = data
+            this.foundData = false
+        })
+    }
+}
+</script>
